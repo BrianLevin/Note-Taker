5dc3d11c3dceca8a7ca6ee685d6f606af8e7853e
Brian Levin <bml201095@gmail.com>
2020-05-09 22:49:27 -0400

    created the function which will read the express module in a json format

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var express = require("express");
 var fs = require('fs');
 var app = express();
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
+
+
+app.use(express.json())
+    .use(express.urlencoded({ extended: false })); 
