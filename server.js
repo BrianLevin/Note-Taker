23bc07bcaedb8986a53f36251510e040d8d1e76b
Brian Levin <bml201095@gmail.com>
2020-05-09 23:03:02 -0400

    created a POST function which includes the data the client sent and then it will be posted onto the html page. Also included is the new client made note pushed onto the array

--- server.js
+++ server.js
@@ -21,3 +21,12 @@ app.get('/api/notes', function (req, res) {
 app.get('/*', function (req, res) {
     res.sendFile(__dirname + '/public/index.html') //put the note on the html page
 });
+
+app.post('/api/notes', function (req, res) {
+    var notes = (JSON.parse(fs.readFileSync(__dirname + '/db/db.json'))); // this is the data the client sent and it is going onto the html page
+
+    notes.push({  // getting the number on notes and then subtracting one
+        title: req.body.title,
+        text: req.body.text,
+        id: notes.length - 1
+    });
