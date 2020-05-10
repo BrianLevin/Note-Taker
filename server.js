2912cc13fbc141c8ea79c117921d0b2477d5ea6c
Brian Levin <bml201095@gmail.com>
2020-05-09 22:56:49 -0400

    created a variable GET function which turns the read file into an object

--- server.js
+++ server.js
@@ -11,3 +11,9 @@ app.use(express.static('public'));
 
 app.get('/notes', function (req, res) {
     res.sendFile(__dirname + '/public/notes.html');
+
+});
+
+app.get('/api/notes', function (req, res) {
+    res.json(JSON.parse(fs.readFileSync(__dirname + '/db/db.json'))); // turn the string into an object
+});
